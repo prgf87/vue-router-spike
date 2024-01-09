15219f1a875e295d611d8c69f6a3eb24431bc44e
Pedro <50713178+prgf87@users.noreply.github.com>
2024-01-09 16:11:54 +0000

    adds loading state to jobs fetch component

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
-    <router-link :to="{ name: 'jobdetails', params: { id: job.id } }">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id">
+      <router-link :to="{ name: 'jobdetails', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading...</p>
   </div>
 </template>
 <script>
