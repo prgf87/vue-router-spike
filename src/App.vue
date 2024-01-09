9e85f941546c8fda18e67347c30e92766dd65f49
Pedro <50713178+prgf87@users.noreply.github.com>
2024-01-09 14:47:30 +0000

    working on routing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/jobs">Jobs</router-link> |
-    <router-link to="/login">Login</router-link>
+    <router-link :to="{ name: 'about' }">About</router-link> |
+    <router-link :to="{ name: 'jobs' }">Jobs</router-link> |
+    <router-link :to="{ name: 'login' }">Login</router-link>
   </nav>
   <router-view />
 </template>
